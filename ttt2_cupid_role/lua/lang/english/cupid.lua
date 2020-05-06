86797811e24265e38f41ccbecec8dd7166b8b4ae
Petermueller1a <57167072+Petermueller1a@users.noreply.github.com>
2020-05-06 14:14:36 +0200

    Add files via upload
    inital upload

---
+++ ttt2_cupid_role/lua/lang/english/cupid.lua
@@ -0,0 +1,21 @@
+L = LANG.GetLanguageTableReference("english")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CUPID.name] = "Cupid"
+L["info_popup_" .. CUPID.name] = [[You are Cupid!
+You can use your Bow to open a GUI to let two people fall in love!
+Cupids Bow is invisible to non-godly beings.
+The bow will become useless after someone died so choose quickly!
+You can also target yourself, which would be weird...]]
+L["body_found_" .. CUPID.abbr] = "This was Cupid."
+L["search_role_" .. CUPID.abbr] = "This person was Cupid!"
+L["target_" .. CUPID.name] = "Cupid"
+L["ttt2_desc_" .. CUPID.name] = [[Cupid can infatuate two people.]]
+L["hilite_win_"..CUPID.name.."s"] ="Love always wins!"
+L["ev_win_"..CUPID.name.."s"] = "Love has won!"
+L["inLovePop_title"] = "You are now in Love with "
+L["inLovePop_text"] = "If they die you will die! You both are now in Team "
+L["inLovePop_title"] = "You are now in Love with "
+L["deathPopup_title"] = "Your loved one died!"
+L["deathPopup_text"] = "You will join them in 5 seconds"
+L["hoverLove"] = "Your one and only"
