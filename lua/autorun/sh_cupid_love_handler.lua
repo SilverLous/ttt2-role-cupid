7d84b38d13e4155a1f1ac4066ac9bbe5275a9aa6
SilverLous <Peter.mueller1a@gmail.com>
2020-05-09 11:30:03 +0200

    removed damge split with cupid

--- lua/autorun/sh_cupid_love_handler.lua
+++ lua/autorun/sh_cupid_love_handler.lua
@@ -78,26 +78,15 @@ net.Receive("Lovedones", function()
 					local attacker = dmginfo:GetAttacker()
 					if not IsValid(attacker) or not attacker:IsPlayer() then return end				
 					local damage = dmginfo:GetDamage()
-					if ply.inLove or (ply:GetRole()==ROLE_CUPID and GetConVar("ttt_cupid_joins_team_lovers"):GetBool()) then
-						if ( not m_bApplyingDamage and GetConVar("ttt_cupid_joins_team_lovers"):GetBool() and lovedones[1]~=lovedones[3] and lovedones[2]~=lovedones[3] ) then
+					if ply.inLove then
+						if ( not m_bApplyingDamage) then                            
 							m_bApplyingDamage = true
-							dmginfo:SetDamage(dmginfo:GetDamage() / 3)
+							dmginfo:SetDamage(dmginfo:GetDamage() / 2)
 							lovedones[1]:TakeDamageInfo( dmginfo )
 							lovedones[2]:TakeDamageInfo( dmginfo )
-							lovedones[3]:TakeDamageInfo( dmginfo )
 							dmginfo:ScaleDamage(0)
 							m_bApplyingDamage = false
 							return
-                        else
-                            if ( not m_bApplyingDamage) then                            
-                                m_bApplyingDamage = true
-                                dmginfo:SetDamage(dmginfo:GetDamage() / 2)
-                                lovedones[1]:TakeDamageInfo( dmginfo )
-                                lovedones[2]:TakeDamageInfo( dmginfo )
-                                dmginfo:ScaleDamage(0)
-                                m_bApplyingDamage = false
-                                return
-                            end
                         end
 					end
 				end)
