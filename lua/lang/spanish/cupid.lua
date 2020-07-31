b84dba242c73afa42fb66b1112f3a78e27ff7853
Tekiad <tekiadp@gmail.cm>
2020-07-31 00:46:11 -0300

    Create cupid.lua
    spanish cupid role translation

---
+++ lua/lang/spanish/cupid.lua
@@ -0,0 +1,26 @@
+L = LANG.GetLanguageTableReference("spanish")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CUPID.name] = "Cupido"
+L["info_popup_" .. CUPID.name] = [[¡Eres Cupido!
+¡Puedes usar tu ballesta para enamorar y unir a dos jugadores!
+Tu arma y flecha son invisibles ante los mortales.
+Tu ballesta será inútil si alguien muere ¡Apúrate!
+También puedes clavarte una flecha a ti mismo, pero sería raro...]]
+L["body_found_" .. CUPID.abbr] = "¡Era Cupido!"
+L["search_role_" .. CUPID.abbr] = "Esta persona era Cupido."
+L["target_" .. CUPID.name] = "Cupido"
+L["ttt2_desc_" .. CUPID.name] = [[Cupido puede enamorar a dos personas.]]
+L["hilite_win_"..CUPID.name.."s"] ="¡El amor siempre gana!"
+L["ev_win_"..CUPID.name.."s"] = "¡El amor triunfó!"
+L["inLovePop_title"] = "Ahora estás enamorado de "
+L["crossBow_title"] = "Le diste correctamente a "
+L["crossBow_text"] = "¡Ahora enamora a otra persona o háztelo a ti mismo con el click secundario!"
+L["inLovePop_title"] = "Ahora estás enamorado de "
+L["inLovePop_text"] = "¡Si ellos mueren, tú también lo harás! Ahora estás en equipo con "
+L["inLovePop_cup_title"] = "El amor está en el aire"
+L["inLovePop_cup_text"] = "Ahora están todos en el equipo "
+L["deathPopup_title"] = "¡Tu enamorado ha muerto!"
+L["deathPopup_text"] = "Te unirás a ellos en 5 segundos"
+L["hoverLove"] = "Tu único/a"
+L["win_" ..CUPID.name .."s"] = "¡Los amantes han ganado!"
