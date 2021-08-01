e76fdcf2667ccd55e0feba2153db633da19ad847
Lukas Gerlach <lukas.gerlach@smail.inf.h-brs.de>
2021-08-01 19:50:21 +0200

    -added print to debug console about the lovers

--- lua/autorun/sh_cupid_love_handler.lua
+++ lua/autorun/sh_cupid_love_handler.lua
@@ -59,6 +59,7 @@ net.Receive("Lovedones", function()
             lovedones[1]:ChatPrint("You are now in Team Lovers")
             lovedones[2]:UpdateTeam(TEAM_CUPID)
             lovedones[2]:ChatPrint("You are now in Team Lovers")
+			PrintMessage(HUD_PRINTCONSOLE, lovedones[1]:Nick().." is now in love with "..lovedones[2]:Nick()..".")
             
             if GetConVar("ttt_cupid_joins_team_lovers"):GetBool() then                      
                 lovedones[3]:UpdateTeam(TEAM_CUPID)
@@ -68,6 +69,7 @@ net.Receive("Lovedones", function()
 		if GetConVar("ttt_cupid_joins_team_lovers"):GetBool() && lovedones[1]:GetTeam() ~= lovedones[3]:GetTeam() then   
 			lovedones[3]:UpdateTeam(lovedones[1]:GetTeam())
             lovedones[3]:ChatPrint("You are now in Team " .. tostring(lovedones[1]:GetTeam()))
+			PrintMessage(HUD_PRINTCONSOLE, lovedones[3]:Nick().." is now also in on it.")
 		end
 		SendFullStateUpdate()
 		net.Start("inLove")
